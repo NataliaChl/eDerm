b8e91aab01ad31596adfe357d66c71b656bca2ec
Natalie <nhlouv@gmai.com>
2022-03-07 00:29:04 +0200

    .

--- src/app/enums/examined-image-response.enum.ts
+++ src/app/enums/examined-image-response.enum.ts
@@ -1,12 +1,12 @@
 export enum ExaminedImageResponse {
  
-MEL = 'Melanoma',
-NV = 'Melanocytic nevus',
+MEL = 'Μελάνωμα (Melanoma)',
+NV = 'Μελανοκυτταρικός σπίλος (Melanocytic nevus)',
 BCC = 'Basal cell carcinoma',
 AK = 'Actinic keratosis',
 BKL = 'Benign keratosis (solar lentigo / seborrheic keratosis / lichen planus-like keratosis)',
 DF = 'Dermatofibroma',
 VASC = 'Vascular lesion',
 SCC = 'Squamous cell carcinoma',
-UNK = 'None of the others'
+UNK = 'Δε βρέθηκε κάποια διάγνωση'
 }
